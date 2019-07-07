1d96b788aac706fab45e7a2a3161721a13767a50
Carbon <carbon.szlachta@gmail.com>
2019-07-07 03:11:21 +0200

    Mbed port

--- src/LoRa.h
+++ src/LoRa.h
@@ -4,29 +4,16 @@
 #ifndef LORA_H
 #define LORA_H
 
-#include <Arduino.h>
-#include <SPI.h>
+#include "mbed.h"
 
-#ifdef ARDUINO_SAMD_MKRWAN1300
-#define LORA_DEFAULT_SPI           SPI1
-#define LORA_DEFAULT_SPI_FREQUENCY 250000
-#define LORA_DEFAULT_SS_PIN        LORA_IRQ_DUMB
-#define LORA_DEFAULT_RESET_PIN     -1
-#define LORA_DEFAULT_DIO0_PIN      -1
-#else
-#define LORA_DEFAULT_SPI           SPI
 #define LORA_DEFAULT_SPI_FREQUENCY 8E6 
-#define LORA_DEFAULT_SS_PIN        10
-#define LORA_DEFAULT_RESET_PIN     9
-#define LORA_DEFAULT_DIO0_PIN      2
-#endif
 
 #define PA_OUTPUT_RFO_PIN          0
 #define PA_OUTPUT_PA_BOOST_PIN     1
 
-class LoRaClass : public Stream {
+class LoRaClass {
 public:
-  LoRaClass();
+  LoRaClass(PinName mosi, PinName miso, PinName sck, PinName ss, PinName rst, PinName dio0);
 
   int begin(long frequency);
   void end();
@@ -49,11 +36,9 @@ public:
   virtual int peek();
   virtual void flush();
 
-#ifndef ARDUINO_SAMD_MKRWAN1300
   void onReceive(void(*callback)(int));
-
   void receive(int size = 0);
-#endif
+
   void idle();
   void sleep();
 
@@ -75,10 +60,8 @@ public:
   void crc() { enableCrc(); }
   void noCrc() { disableCrc(); }
 
-  byte random();
+  uint8_t random();
 
-  void setPins(int ss = LORA_DEFAULT_SS_PIN, int reset = LORA_DEFAULT_RESET_PIN, int dio0 = LORA_DEFAULT_DIO0_PIN);
-  void setSPI(SPIClass& spi);
   void setSPIFrequency(uint32_t frequency);
 
   void dumpRegisters(Stream& out);
@@ -99,20 +82,15 @@ private:
   void writeRegister(uint8_t address, uint8_t value);
   uint8_t singleTransfer(uint8_t address, uint8_t value);
 
-  static void onDio0Rise();
-
 private:
-  SPISettings _spiSettings;
-  SPIClass* _spi;
-  int _ss;
-  int _reset;
-  int _dio0;
+  SPI _spi;
+  DigitalOut _ss;
+  DigitalOut _reset;
+  InterruptIn _dio0;
   long _frequency;
   int _packetIndex;
   int _implicitHeaderMode;
   void (*_onReceive)(int);
 };
-
-extern LoRaClass LoRa;
 
 #endif
